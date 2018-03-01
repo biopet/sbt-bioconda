fe9a7dfe28360fdc77dc8f06612d2ed428772b6d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-03-01 16:00:48 +0100

    start on biocondaplugin scaffold

--- src/main/scala/nl/biopet/bioconda/BiocondaPlugin.scala
+++ src/main/scala/nl/biopet/bioconda/BiocondaPlugin.scala
@@ -1,12 +1,19 @@
 package nl.biopet.bioconda
 
-import sbt._
+import sbt.{Def, _}
 import com.typesafe.sbt.GitPlugin
 import ohnosequences.sbt.SbtGithubReleasePlugin
+import com.typesafe.sbt.SbtGit.GitKeys
+import com.typesafe.sbt.git.GitRunner
+import GitKeys.{gitBranch,gitRemoteRepo}
 object BiocondaPlugin extends AutoPlugin {
   override def trigger: PluginTrigger = noTrigger
   override def requires: Plugins = GitPlugin && SbtGithubReleasePlugin
 
   object autoImport extends BiocondaKeys
   import autoImport._
+
+  override def projectSettings: Seq[Def.Setting[_]] = super.projectSettings
+
+  override def globalSettings: Seq[Def.Setting[_]] = super.globalSettings
 }
