166f69b5c86c65dfc1622b960b92af85e39d271d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-03-14 16:35:33 +0100

    changed updated repository to make things pull-requestable

--- src/main/scala/nl/biopet/bioconda/BiocondaPlugin.scala
+++ src/main/scala/nl/biopet/bioconda/BiocondaPlugin.scala
@@ -10,20 +10,25 @@ import GitKeys.{gitBranch, gitRemoteRepo}
 
 object BiocondaPlugin extends AutoPlugin {
   override def trigger: PluginTrigger = noTrigger
+
   override def requires: Plugins = GitPlugin && SbtGithubReleasePlugin
 
   object autoImport extends BiocondaKeys
+
   import autoImport._
 
   override def projectSettings: Seq[Def.Setting[_]] = Def.settings(
     biocondaBranch := normalizedName.value
   )
+
   override def globalSettings: Seq[Def.Setting[_]] = Def.settings(
     biocondaMainGitUrl := "https://github.com/bioconda/bioconda-recipes.git",
     biocondaMainBranch := "master",
     biocondaUpdatedRepository := updatedRepo(biocondaRepository,
                                              biocondaGitUrl,
-                                             biocondaBranch).value
+                                             biocondaBranch,
+                                             biocondaMainGitUrl,
+                                             biocondaMainBranch).value
   )
 
   /*
@@ -32,30 +37,33 @@ object BiocondaPlugin extends AutoPlugin {
   private def updatedRepo(
       repo: SettingKey[File],
       remote: SettingKey[String],
-      branch: SettingKey[String]): Def.Initialize[Task[File]] =
+      branch: SettingKey[String],
+      upstream: SettingKey[String],
+      upstreamBranch: SettingKey[String]): Def.Initialize[Task[File]] =
     Def.task[File] {
       val local = repo.value
       val git = GitKeys.gitRunner.value
       val s = streams.value
+
+      // Make sure there is a git repo checked out at the desired branch
       git.updated(remote = remote.value,
-        cwd = local,
-        branch = Some(branch.value),
-        log = s.log)
-      local
-    }
-  private def setUpstream(repo: SettingKey[File],
-                          upstream: SettingKey[String]
-                               ): Def.Initialize[Task[File]] =
-    Def.task[File] {
-      val git = GitKeys.gitRunner.value
-      val local = repo.value
-      val remotes: Array[String] = git.apply("remote")(local,streams.value.log).split("\\n")
+                  cwd = local,
+                  branch = Some(branch.value),
+                  log = s.log)
+
+      // Make sure the upstream git url is added
+      val remotes: Array[String] =
+        git.apply("remote")(local, streams.value.log).split("\\n")
       if (remotes.contains("upstream")) {
-        git.apply("remote", "set-url","upstream",upstream.value)
+        git.apply("remote", "set-url", "upstream", upstream.value)
+      } else {
+        git.apply("remote", "add", "upstream", upstream.value)
       }
-      else {
-        git.apply("remote", "add", "upstream",upstream.value)
-      }
+
+      // rebase the current branch on upstream main, so a pull request can be made
+      // from this branch.
+      git.apply("fetch", "upstream")
+      git.apply("rebase", "upstream", upstreamBranch.value)
       local
     }
 }
