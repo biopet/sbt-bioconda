2dae5c74226aa677c8549063b144c2886545af13
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-04-05 11:46:36 +0200

    add skip errors key

--- src/main/scala/nl/biopet/bioconda/BiocondaPlugin.scala
+++ src/main/scala/nl/biopet/bioconda/BiocondaPlugin.scala
@@ -74,7 +74,8 @@ object BiocondaPlugin extends AutoPlugin {
     biocondaPullRequestBody := defaultPullRequestBody.value,
     biocondaPullRequestTitle := defaultPullRequestTitle.value,
     biocondaPullRequest := createPullRequest.value,
-    biocondaRelease := releaseProcedure().value
+    biocondaRelease := releaseProcedure().value,
+    biocondaSkipErrors := false
   )
 
   private def releaseProcedure(): Def.Initialize[Task[Unit]] =
